004ee06344dc3a81662474c2c030a73c0495993a
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-06-23 12:48:51 +0300

    initial commit

---
+++ src/test/java/com/roze/book_recommendation_app/BookRecommendationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roze.book_recommendation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
