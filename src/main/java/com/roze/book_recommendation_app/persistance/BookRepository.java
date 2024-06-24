ca3ad65f3e3c4332c0c9c9781e17b07a0b398465
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-06-24 13:46:19 +0300

    book: added query methods to search books by title and category

--- src/main/java/com/roze/book_recommendation_app/persistance/BookRepository.java
+++ src/main/java/com/roze/book_recommendation_app/persistance/BookRepository.java
@@ -1,11 +1,16 @@
 package com.roze.book_recommendation_app.persistance;
 
 import com.roze.book_recommendation_app.persistance.entity.Book;
+import com.roze.book_recommendation_app.persistance.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     boolean existsByIsbnIgnoreCase(String isbn);
+    List<Book> findByTitleIgnoreCase(String bookName);
+    List<Book> findByCategory(Category category);
 }
