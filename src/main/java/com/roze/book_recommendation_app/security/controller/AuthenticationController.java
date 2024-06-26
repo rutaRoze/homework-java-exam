705c064e506707705b129c638d39f25d29d1a43b
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-06-26 10:59:04 +0300

    updates-for-front: added crossorigin to auth contrller

--- src/main/java/com/roze/book_recommendation_app/security/controller/AuthenticationController.java
+++ src/main/java/com/roze/book_recommendation_app/security/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
 
-    @Autowired
     private AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
